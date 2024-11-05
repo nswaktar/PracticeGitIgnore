2c28b3e1189a90b51ab4f5c55cf0ec44da13c944
Aktar <apaktar@gmail.com>
2024-11-05 16:10:35 +0600

    Initial commit: Add PracticeGitIgnore project with .gitignore and README

---
+++ src/PracticeGitIgnore.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PracticeGitIgnore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the message:");
+        String message1= scanner.nextLine();
+        String message2= scanner.nextLine();
+        System.out.println(message1+message2);
+        scanner.close();
+        // remove comments and add your code here
+    }
+}
