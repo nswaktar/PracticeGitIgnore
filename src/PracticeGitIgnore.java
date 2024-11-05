5e075912f73f368fea1942a96eda54c9a8b9c355
Aktar <apaktar@gmail.com>
2024-11-05 16:48:49 +0600

    refactoring

--- src/PracticeGitIgnore.java
+++ src/PracticeGitIgnore.java
@@ -3,10 +3,9 @@ public class PracticeGitIgnore {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the message:");
-        String message1= scanner.nextLine();
-        String message2= scanner.nextLine();
-        System.out.println(message1+message2);
+        String message1 = scanner.nextLine();
+        String message2 = scanner.nextLine();
+        System.out.println(message1.concat(message2));
         scanner.close();
-        // remove comments and add your code here
     }
 }
