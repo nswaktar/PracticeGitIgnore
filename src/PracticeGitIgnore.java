f67db317b77c16f87000352413ce347ce894ab50
Aktar <apaktar@gmail.com>
2024-11-05 16:59:12 +0600

    minor change

--- src/PracticeGitIgnore.java
+++ src/PracticeGitIgnore.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class PracticeGitIgnore {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the message:");
+        System.out.println("Enter the message: ");
         String message1 = scanner.nextLine();
         String message2 = scanner.nextLine();
         System.out.println(message1.concat(message2));
